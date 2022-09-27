34991219eea883216d61afcd30663ad737f7c1df
Gumenyik <78902763+Gumenyik@users.noreply.github.com>
2022-09-27 12:06:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
